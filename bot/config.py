001f2f6c0a8157c2e1d695a58aef4c4392e1d45c
jishankai <jishankai@gmail.com>
2023-03-29 12:03:35 +0800

    fix config

--- bot/config.py
+++ bot/config.py
@@ -8,13 +8,6 @@ config_dir = Path(__file__).parent.parent.resolve() / "config"
 with open(config_dir / "config.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
-# load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
-
-# load filtered words
-with open(config_dir / "filtered.txt", 'r') as f:
-    filtered_words = f.read().splitlines()
-
 # config parameters
 telegram_token = config_yaml["telegram_token"]
 bot_id = config_yaml["bot_id"]
